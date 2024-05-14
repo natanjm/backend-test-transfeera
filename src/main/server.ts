7572488f67501fa2f62af182c895c6bc5f798fdf
natan-grupo-voa <natanmuller@grupovoa.com.br>
2024-05-13 21:48:30 -0300

    feat: server setup

---
+++ src/main/server.ts
@@ -0,0 +1,12 @@
+
+import { MongoHelper } from '../infra/db/mongo/helpers/mongo-helper';
+import env from './config/env';
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => {
+      console.log('Server running at http://localhost:5050')
+    });
+  })
+  .catch(console.error)
