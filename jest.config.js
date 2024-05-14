a6d52ad368c7c73beca4680f84978088a0940e28
natan-grupo-voa <natanmuller@grupovoa.com.br>
2024-05-13 21:49:50 -0300

    feat: jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
-  ],
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  },
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb'
-}
+	roots: ['<rootDir>/src'],
+	collectCoverageFrom: [
+		'<rootDir>/src/**/*.ts',
+		'!<rootDir>/src/main/**',
+	],
+	transform: {
+		'.+\\.ts$': 'ts-jest',
+	},
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	testEnvironment: 'node',
+	preset: '@shelf/jest-mongodb',
+};
